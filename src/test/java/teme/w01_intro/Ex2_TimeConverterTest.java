3ad691952f5b2c373239a9abae768e929d4a219d
Cristi Macoviciuc <cristi.macoviciuc@gmail.com>
2020-02-13 14:13:11 +0200

    update w1 tests

--- src/test/java/teme/w01_intro/Ex2_TimeConverterTest.java
+++ src/test/java/teme/w01_intro/Ex2_TimeConverterTest.java
@@ -6,7 +6,6 @@ import teme.util.plugin.Grade;
 import teme.util.plugin.GradeRunner;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
 import static teme.w01_intro.Ex2_TimeConverter.convertToMilliseconds;
 
 /**
@@ -17,7 +16,7 @@ public class Ex2_TimeConverterTest {
 
     @Test
     @Grade(2)
-    public void testRangeValidation_invalidValuesReturnMinusOne() {
+    public void testRangeValidations() {
         assertEquals(-1, convertToMilliseconds(-1, 0, 0));
         assertEquals(-1, convertToMilliseconds(24, 0, 0));
         assertEquals(-1, convertToMilliseconds(0, -1, 0));
@@ -28,18 +27,37 @@ public class Ex2_TimeConverterTest {
 
     @Test
     @Grade(1)
-    public void testRangeValidation_validValuesDoNotReturnMinusOne() {
-        assertNotEquals(-1, convertToMilliseconds(0, 0, 0));
-        assertNotEquals(-1, convertToMilliseconds(23, 59, 59));
+    public void testZero() {
+        assertEquals(0, convertToMilliseconds(0, 0, 0));
     }
 
     @Test
-    @Grade(5)
-    public void testConversionOfValidValues() {
-        assertEquals(0, convertToMilliseconds(0, 0, 0));
+    @Grade(1)
+    public void testJustSeconds() {
+        assertEquals(1_000, convertToMilliseconds(0, 0, 1));
+        assertEquals(30_000, convertToMilliseconds(0, 0, 30));
+        assertEquals(59_000, convertToMilliseconds(0, 0, 59));
+    }
+
+    @Test
+    @Grade(1)
+    public void testJustMinutes() {
+        assertEquals(60_000, convertToMilliseconds(0, 1, 0));
+        assertEquals(300_000, convertToMilliseconds(0, 5, 0));
+    }
+
+    @Test
+    @Grade(1)
+    public void testJustHours() {
+        assertEquals(3_600_000, convertToMilliseconds(1, 0, 0));
         assertEquals(7_200_000, convertToMilliseconds(2, 0, 0));
-        assertEquals(180_000, convertToMilliseconds(0, 3, 0));
-        assertEquals(4_000, convertToMilliseconds(0, 0, 4));
+        assertEquals(82_800_000, convertToMilliseconds(23, 0, 0));
+    }
+
+    @Test
+    @Grade(2)
+    public void testCombined() {
+        assertEquals(3_723_000, convertToMilliseconds(1, 2, 3));
         assertEquals(18_367_000, convertToMilliseconds(5, 6, 7));
         assertEquals(86_399_000, convertToMilliseconds(23, 59, 59));
     }
