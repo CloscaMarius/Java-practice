1adb89aec9d5292b8f3ec0e0756836b0eff06918
Cristi Macoviciuc <cristi.macoviciuc@gmail.com>
2020-02-13 12:25:30 +0200

    update template

--- src/main/java/teme/w02_flow/Ex1_FizzBuzz.java
+++ src/main/java/teme/w02_flow/Ex1_FizzBuzz.java
@@ -11,7 +11,14 @@ class Ex1_FizzBuzz {
         //TODO
     }
 
+
     public static void main(String[] args) {
+        System.out.println(" 2 -> " + fizzBuzz(2));
+        System.out.println(" 6 -> " + fizzBuzz(6));
+        System.out.println("10 -> " + fizzBuzz(10));
+        System.out.println("30 -> " + fizzBuzz(30));
+
+        System.out.println("\nAll up to 20:");
         printAllFizzBuzzUpTo(20);
     }
 }
