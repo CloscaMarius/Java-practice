a2b191052e623613faa9db08fed9da3e82074f37
Cristi Macoviciuc <cristi.macoviciuc@gmail.com>
2020-02-24 12:19:27 +0200

    tema2 - add new manual tests

--- src/main/java/teme/w02_flow/Ex5_Fibonacci.java
+++ src/main/java/teme/w02_flow/Ex5_Fibonacci.java
@@ -10,7 +10,6 @@ class Ex5_Fibonacci {
         int fib = 0;
         if (n > 2) {
             for (int i = 1; i < n; i++) {
-
                 fib = f0 + f1;
                 f0 = f1;
                 f1 = fib;
@@ -30,8 +29,6 @@ class Ex5_Fibonacci {
         for (int i = 0; i < n; i++) {
             fibonacciNumbers[i] += fibonacci(i);
         }
-
-
         return fibonacciNumbers;
     }
 
@@ -48,5 +45,20 @@ class Ex5_Fibonacci {
         System.out.println("fibonacciSequence(1): " + Arrays.toString(fibonacciSequence(1)));
         System.out.println("fibonacciSequence(5): " + Arrays.toString(fibonacciSequence(5)));
         System.out.println("fibonacciSequence(25): " + Arrays.toString(fibonacciSequence(25)));
+
+        System.out.println("\n--- EXECUTION TIMES --- ");
+
+        //TODO: feel free to increase max limit beyond 40 (to like 100) if you feel your code is fast enough!
+        for (int i = 20; i <= 40; i += 2) {
+            final int n = i;
+            printTime("fibonacciSequence(" + i + ")", () -> fibonacciSequence(n));
+        }
+    }
+
+    private static void printTime(String description, Runnable codeToRun) {
+        long startTime = System.currentTimeMillis();
+        codeToRun.run();
+        long stopTime = System.currentTimeMillis();
+        System.out.println(description + " - took: " + (stopTime - startTime) + " ms");
     }
 }
